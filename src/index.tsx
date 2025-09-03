c4a01c2f1b6acf13776078ab99b25ff07a82e39f
Stephan van Stekelenburg <stephan@hayman.io>
2025-09-03 21:25:27 +0200

    feat: splash screen

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import { render } from "@opentui/solid";
 render(() => (
   <box alignItems="center" justifyContent="center" flexGrow={1}>
     <box justifyContent="center" alignItems="flex-end">
-      <ascii_font font="tiny" text="OpenTUI" />
-      <text attributes={TextAttributes.DIM}>What will you build?</text>
+      <ascii_font font="tiny" text="OpenCAL" />
+      <text attributes={TextAttributes.DIM}>What will you schedule?</text>
     </box>
   </box>
 ));
