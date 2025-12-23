6be7be8316fbf3cc2ecb9303797054993e391015
Stephan van Stekelenburg <stephan@hayman.io>
2025-12-23 22:58:58 +0100

    feat: view mode selector styling

--- src/tui/components/view-selector.tsx
+++ src/tui/components/view-selector.tsx
@@ -1,23 +1,36 @@
+import { For } from "solid-js";
 import { CalendarViewType } from "../../models";
-import { t, underline } from "@opentui/core";
+import { useTheme } from "../context/theme";
+
+const views = [
+  { type: CalendarViewType.Month, shortcut: 'm', label: 'onth' },
+  { type: CalendarViewType.Week, shortcut: 'w', label: 'eek' },
+  { type: CalendarViewType.Day, shortcut: 'd', label: 'ay' },
+] as const;
 
 const ViewSelector = (props: { currentViewMode: CalendarViewType }) => {
-  const views = [
-    { type: CalendarViewType.Month, shortcut: 'm', label: 'onth' },
-    { type: CalendarViewType.Week, shortcut: 'w', label: 'eek' },
-    { type: CalendarViewType.Day, shortcut: 'd', label: 'ay' },
-  ];
+  const { theme } = useTheme();
 
   return (
     <box flexDirection="row" gap={1}>
-      {views.map(view => (
-        <text>
-          {props.currentViewMode === view.type 
-            ? t`[${underline(view.shortcut)}]${view.label}`
-            : `[${view.shortcut}]${view.label}`
-          }
-        </text>
-      ))}
+      <For each={views}>
+        {(view) => {
+          const isActive = () => props.currentViewMode === view.type;
+          return (
+            <box flexDirection="row">
+              <box backgroundColor={
+                isActive() 
+                ? theme.primary 
+                : theme.border}>
+                <text fg={isActive() ? theme.text : theme.primary }>{view.shortcut}</text>
+              </box>
+              <box backgroundColor={theme.backgroundElement}>
+                <text>{view.label}</text>
+              </box>
+            </box>
+          );
+        }}
+      </For>
     </box>
   );
 };
